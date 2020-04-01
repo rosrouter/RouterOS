9adffa36060854394076337825e2ed077bb2c080
xxddpac <cn2fastsecure@gmail.com>
2020-04-01 22:20:48 +0800

    chanage dev

--- network/adminx.py
+++ network/adminx.py
@@ -1,11 +1,7 @@
 from xadmin.plugins.actions import BaseActionView
 import xadmin
 from xadmin import views
-<<<<<<< HEAD
-from network.models import L2TP, UserManage, NetworkManage
-=======
 from network.models import RosRouter, UserManage, VPNInfo
->>>>>>> master
 
 
 class GlobalSetting(object):
@@ -23,26 +19,6 @@ xadmin.site.register(views.CommAdminView, GlobalSetting)
 xadmin.site.register(views.BaseAdminView, BaseSetting)
 
 
-<<<<<<< HEAD
-class L2TPAdmin(object):
-    list_display = ['username', 'password', 'status']
-
-    # list_filter = []
-    # search_fields = []
-    # actions = []   # 执行操作
-    # preserve_filters = True
-    # list_editable = []  # 可直接编辑
-    # def queryset(self):
-    #     """函数作用：使当前登录的用户只能看到自己负责的设备"""
-    #     qs = super(NetworkAdmin, self).queryset()
-    #     if self.request.user.is_superuser:
-    #         return qs
-    #     return Network.objects.filter(member__contains=self.request.user.username)
-
-
-class NetworkAdmin(object):
-    list_display = ['ip', 'vpns']
-=======
 class VPNAdmin(object):
     list_display = ['vpn_user', 'vpn_pwd', 'status']
 
@@ -62,19 +38,12 @@ class VPNAdmin(object):
 
 class RosRouterAdmin(object):
     list_display = ['ip', 'ros_user', 'ros_pwd']
->>>>>>> master
 
 
 class UserAdmin(object):
     list_display = ['user', 'device']
 
 
-<<<<<<< HEAD
-xadmin.site.register(L2TP, L2TPAdmin)
-xadmin.site.register(NetworkManage, NetworkAdmin)
-xadmin.site.register(UserManage, NetworkAdmin)
-=======
 xadmin.site.register(VPNInfo, VPNAdmin)
 xadmin.site.register(RosRouter, RosRouterAdmin)
 xadmin.site.register(UserManage, UserAdmin)
->>>>>>> master
--- network/models.py
+++ network/models.py
@@ -3,27 +3,6 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-<<<<<<< HEAD
-class L2TP(models.Model):
-    username = models.CharField(verbose_name='L2TP用户名', max_length=60, null=False, default='')
-    password = models.CharField(verbose_name='L2TP密码', max_length=60, null=False, default='')
-    status = models.CharField(verbose_name='VPN状态', max_length=60, null=False, default='')
-
-    class Meta:
-        verbose_name = "VPN信息"
-        verbose_name_plural = verbose_name
-        db_table = "l2tp"
-
-
-class NetworkManage(models.Model):
-    ip = models.GenericIPAddressField(verbose_name='ip地址', max_length=30, null=False, default='')
-    vpns = models.ManyToManyField(L2TP, verbose_name='下属VPN', blank=True)
-
-    class Meta:
-        verbose_name = "网络设备关联VPN"
-        verbose_name_plural = verbose_name
-        db_table = "network"
-=======
 class RosRouter(models.Model):
     ip = models.GenericIPAddressField(verbose_name='ip地址', null=True, blank=True)
     ros_user = models.CharField(verbose_name='ros用户', max_length=100, null=False, default='')
@@ -48,16 +27,11 @@ class VPNInfo(models.Model):
         verbose_name = "VPN信息"
         verbose_name_plural = verbose_name
         db_table = "vpn_info"
->>>>>>> master
 
 
 class UserManage(models.Model):
     user = models.ForeignKey(User, verbose_name='系统用户', blank=True, on_delete=False)
-<<<<<<< HEAD
-    device = models.ManyToManyField(NetworkManage, verbose_name='下属设备', blank=True)
-=======
     device = models.ManyToManyField(RosRouter, verbose_name='下属ros路由器', blank=True)
->>>>>>> master
 
     class Meta:
         verbose_name = "用户管理设备"
