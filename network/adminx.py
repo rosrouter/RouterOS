75ecd800b7d34923f70936695faeaf62c3701244
yangzhongding <1454591329@qq.com>
2020-04-06 16:29:56 +0800

    feat:Control the way when Vpn model save and delete info

--- network/adminx.py
+++ network/adminx.py
@@ -1,6 +1,7 @@
 import xadmin
 from xadmin import views
 from network.models import RosRouter, UserManage, VPNInfo
+import logging
 
 
 class GlobalSetting(object):
@@ -24,7 +25,7 @@ class VPNAdmin(object):
     actions = []  # 执行操作
     preserve_filters = True
     list_editable = []  # 可直接编辑
-    list_per_page = 15 # 分页
+    list_per_page = 15  # 分页
 
     def queryset(self):
         """函数作用：使当前登录的用户只能看到自己负责的设备"""
@@ -32,6 +33,20 @@ class VPNAdmin(object):
         if self.request.user.is_superuser:
             return qs
         return VPNInfo.objects.filter(ros__usermanage__user=self.request.user)
+
+    def save_models(self):
+        """在增加或者修改vpn信息的时候触发"""
+        obj = self.new_obj
+        if obj.id is None:  # 新增的时候
+            print('create a new vpn')
+        else:  # 修改的时候
+            print('update a vpn info')
+        super(VPNAdmin, self).save_models()
+
+    def delete_model(self):
+        """删除VPN的时候触发"""
+        print('delete a vpn')
+        super(VPNAdmin, self).delete_model()
 
 
 class RosRouterAdmin(object):
